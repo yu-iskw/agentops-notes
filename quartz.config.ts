d963d6766b7d6b4560cc0a1c90759bda9844b58f
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2025-06-23 17:01:09 +0900

    fix cross-references
    Signed-off-by: Yu Ishikawa <yu-iskw@users.noreply.github.com>

--- quartz.config.ts
+++ quartz.config.ts
@@ -69,7 +69,11 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({
+        markdownLinkResolution: "shortest",
+        prettyLinks: true,
+        openLinksInNewTab: false
+      }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
